ece34db80ac5d6f5f055ab9c82b0c45e7bb77e86
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-01-07 18:00:50 -0500

    worked on initializing timer function and loading first question

--- Assets/script3.js
+++ Assets/script3.js
@@ -1,51 +1,97 @@
 //global variables
-//!I hear it's better not to have global variables, but to have them in the functions themselves. How do local variables work with querySelectors?
 const startButtonClicked = document.querySelector("#start-btn");
-const timerEl = document.getElementsByClassName(".timer");
+const timerEl = document.querySelector(".timer");
 const nextBtn = document.querySelector("#nxt-btn");
 const restartBtn = document.querySelector("#replay-btn");
-const quizQuestions = "Commonly used data types do NOT include ____."; //!should I link this via className, or better to write all the questions here as an array?
+const questionsContainerEL = document.querySelectorAll("#quiz-shell");
+const choicesEL = document.getElementById("answerChoices");
+let timeInterval;
+let timer = 59;
+let currentQuestion;
+let currentQuestionIndex = 0;
+let questions = [
+    {
+        question: "Commonly used data types do NOT include ____.",
+        answerChoices: ["strings", "booleans", "alerts", "numbers"],
+        correctAnswer: "alerts",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _____.",
+        answerChoices: ["parenthesis", "curly brackets", "quotes", "square brackets"],
+        correctAnswer: "parenthesis",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        answerChoices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correctAnswer: "all of the above",
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        answerChoices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        correctAnswer: "quotes",
+    },
+];
+
 
 //starts Game
-function initializeGame(event) {
-  //! when startButton is pressed then a countdownTimer starts backwards from 60 seconds and question is loaded--do I need a function for "startButton" under this initializeGame function??) Also, what about putting load question as it's own function (outside initializeGame), and how would doing so affect the above question about startButton etc?
-  event.preventDefault();
-  startButtonClicked.innerHTML = "Pressed!";
-  console.log("Countdown!");
-  return initializeGame; //Should this be returned after the countDownTimer function?
-}
+function initializeTimer() {
+    //kicks off game by starting countdown timer once start button is pressed
+  timeInterval = setInterval(countDownTimer, 1000);
+  // let startContainerEL = document.getElementsByClassName("start-container");
+//   startContainerEL.setAttribute("class", "hide");
+//   questionsContainerEL.removeAttribute("class");
+   fetchNewQuestion();
 
-function countDownTimer(event) { //!Doesn't work! See console log
-  // decreases countdown timer by 1 if it meets following conditions
-  event.preventDefault();
-  let timer = 60;
-  let timeInterval = setInterval(startGame);
-  {
-    if (timer > 1) {
-      timerEl.textContent = timer + "seconds remaining";
-      timer--;
-    } else if (timer === 1) {
-      //changes 'seconds' to 'second' in the singular
-      timerEl.textContent = timer + "second remaining";
-      timer--;
-    } else {
-      //doesn't let timer go below zero
-      timerEl.textContent = "";
+    function countDownTimer() {
+    // decreases countdown timer by 1 if it meets following conditions
+        if (timer > 1) {
+        timerEl.textContent = timer + "  seconds remaining";
+        timer--;
+        } else if (timer === 1) {
+    //changes 'seconds' to 'second' in the singular
+        timerEl.textContent = timer + "  second remaining";
+        timer--;
+        clearCountDown()
+        }
     }
+    function clearCountDown() {
+    // return clearCountDown;
+        clearInterval(timeInterval);
+        if(timerEl.textContent = "") {
+        }
   }
-
-  function clearCountDown() { //!still don't know if this works
-    clearInterval(timeInterval);
-    return clearCountDown;
-  }
-
-  //Loads Next Question
-function Questions() { //!Doesn't work!
-    quizQuestions
-  return quizQuestions;
-}
-// console.log(Questions())
 }
 
-startButtonClicked.addEventListener("click", startGame);
-startButtonClicked.addEventListener("click", countDownTimer);
+//not functioning properly from here below
+  function  fetchNewQuestion(choice) {
+    currentQuestion = questions[currentQuestionIndex];
+    let questionEl = document.getElementById("question"); //!check document type. different things happen in console if different document types are selected. none of them bring up other questions though...
+    questionEl.innerText = currentQuestion.question;
+    console.log(questionEl)
+    choicesEL.innerHTML = "";
+    for(let i=0; i<currentQuestion.answerChoices.length; i++) {
+    //!is there an easier way to do this for loop without creating these attributes below??
+        let choiceSelected = currentQuestion.answerChoices[i]
+        let choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("class", "choiceSelected");
+        choiceBtn.setAttribute("value", choiceSelected);
+        choiceBtn.textContent = choiceSelected;
+        choicesEL.appendChild(choiceBtn);
+    }
+    }
+
+// function evaluateAnswer(event) {
+//     //tbd
+//     if (correctAnswer==true) {
+//         return fetchNewQuestion
+//     }
+//     else {
+//         alert("try again");
+//         //!how do I make the timer reduce by 10seconds as penalty?
+//     }
+// }
+
+// function restartGame(){
+// }
+startButtonClicked.addEventListener("click", initializeTimer);
+// answerChoicesClicked.addEventListener("click", evaluateAnswer);
